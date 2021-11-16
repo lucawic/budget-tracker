902aae81741ec05bf051eef903770e68d47b8b77
lucawic <lucaswick@email.arizona.edu>
2021-11-15 21:52:01 -0700

    deploying to heroku and reconnecting db

--- server.js
+++ server.js
@@ -16,10 +16,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget_tracker",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
